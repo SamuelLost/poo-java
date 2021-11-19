fa540517cf80e027876859fc714fa26d699bb4ca
SamuelLost <samuelhenriq12@gmail.com>
2021-11-19 18:21:15 -0300

    Update documentation and create project

---
+++ Trabalho06_UML&RelacObjs_AP1/LapiseiraGrafite/src/Grafite.java
@@ -0,0 +1,8 @@
+class Grafite {
+    float calibre;
+    String dureza;
+    int tamanho;
+    Grafite(float calibre, String dureza, int tamanho);
+    String toString();
+    int desgastePorFolha();
+}
---
+++ Trabalho06_UML&RelacObjs_AP1/LapiseiraGrafite/src/Lapiseira.java
@@ -0,0 +1,9 @@
+class Lapiseira {
+    float calibre;
+    Grafite grafite;
+    Lapiseira(float calibre);
+    String toString();
+    boolean inserir(Grafite grafite);
+    Grafite remover();
+    void escrever();
+}
