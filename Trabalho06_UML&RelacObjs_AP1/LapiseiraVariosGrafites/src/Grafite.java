53b502405b8ff7be3e2ef73b95211fc22d3ee428
SamuelLost <samuelhenriq12@gmail.com>
2021-11-20 01:02:19 -0300

    pass test :heavy_check_mark:

--- Trabalho06_UML&RelacObjs_AP1/LapiseiraVariosGrafites/src/Grafite.java
+++ Trabalho06_UML&RelacObjs_AP1/LapiseiraVariosGrafites/src/Grafite.java
@@ -1,8 +1,57 @@
 class Grafite {
-    float calibre;
-    String dureza;
-    int tamanho;
-    Grafite(float calibre, String dureza, int tamanho);
-    String toString();
-    int desgastePorFolha();
+    float calibre; //espessura do grafite
+    String dureza; //dureza do grafite
+    int tamanho; //tamanho
+
+    /**
+     * Construtor da classe
+     * @param calibre - inteiro para espessura do grafite
+     * @param dureza - string para dureza do grafite
+     * @param tamanho - inteiro para o tamanho
+     */
+    Grafite(float calibre, String dureza, int tamanho) {
+        this.calibre = calibre;
+        this.dureza = dureza;
+        this.tamanho = tamanho;
+    }
+    /**
+     * Método toString para imprimir no formato
+     */
+    public String toString() {
+        return String.format("[%.1f:%s:%d]", this.calibre, this.dureza, this.tamanho);
+    }
+    
+    /**
+     * Métodos getters
+     * @return retornam o calibre e o tamanho
+     */
+    public float getCalibre() {
+        return calibre;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    /**
+     * Método para setar o tamanho do grafite na hora de escrever
+     * @param tamanho
+     */
+    public void setTamanho(int tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    /**
+     * Método que analisa a dureza do grafite e 
+     * retorna quanto ele é gasto por forma
+     * @return - inteiro que corresponde ao gasto
+     */
+    public int desgastePorFolha() {
+        if(this.dureza.equals("HB")) return 1;
+        if(this.dureza.equals("2B")) return 2;
+        if(this.dureza.equals("4B")) return 4;
+        if(this.dureza.equals("6B")) return 6;
+        return 0;
+     }
+    
 }
--- Trabalho06_UML&RelacObjs_AP1/LapiseiraVariosGrafites/src/Lapiseira.java
+++ Trabalho06_UML&RelacObjs_AP1/LapiseiraVariosGrafites/src/Lapiseira.java
@@ -1,11 +1,99 @@
+import java.util.ArrayList;
+import java.util.Locale;
+
 class Lapiseira {
-    float calibre;
-    Grafite bico;
-    ArrayList<Grafite> tambor;
-    Lapiseira(float calibre);
-    String toString();
-    boolean inserir(Grafite grafite);
-    Grafite remover();
-    boolean puxar();
-    void escrever();
+    private float calibre; //espessura que a lapiseira comporta
+    private Grafite bico; // instancia de Grafite representando o bico
+    private ArrayList<Grafite> tambor; //ArrayList dos grafites reservas
+    private final int TAMANHO_MIN = 10;
+    Lapiseira(float calibre) {
+        this.calibre = calibre;
+        tambor = new ArrayList<>();
+    }
+    
+    public String toString() {
+        Locale.setDefault(Locale.US);
+        if(bico == null) {
+            String out = "calibre: " + this.calibre + ", bico: []" + ", tambor: {";
+            if(tambor != null) {
+                for(Grafite a : tambor) {
+                    out += a.toString();
+                }
+            }
+            
+            out += "}";
+            return out;
+        }
+
+        String out = "calibre: " + this.calibre + ", bico: " + bico.toString()
+            + ", tambor: {";
+        if(tambor != null) {
+            for(Grafite a : tambor) {
+                out += a.toString();
+            }
+        }
+        
+        out += "}";
+        return out;
+    }
+
+    /**
+     * Método booleano para fazer a inserção de um grafite
+     * na lapiseira. A lapiseira não pode estar ocupada por
+     * outro grafite e o calibre da lapiseira e o grafite a ser
+     * inserido devem corresponder, caso contrário não é 
+     * possível inserir.
+     * @param grafite - instância da classe Grafite
+     * @return true caso a inserção seja feita com sucesso, false 
+     * caso contrário.
+     */
+    boolean inserir(Grafite grafite) {
+        if((grafite.getCalibre() != this.calibre)) {
+            System.out.println("fail: calibre incompativel");
+            return false;
+        }
+        this.tambor.add(grafite);
+        return true;
+    }
+    Grafite remover() {
+        Grafite aux = bico;
+        bico = null;
+        return aux;
+    }
+    boolean puxar() {
+        if(bico != null) {
+            System.out.println("fail: ja existe grafite no bico");
+            return false;
+        } else {
+            if(tambor.isEmpty()) {
+                System.out.println("fail: nao existe grafite no tambor");
+                return false;
+            }
+            bico = tambor.remove(0);
+            return true;
+        }
+    }
+    void escrever() {
+        if(this.bico == null) {
+            System.out.println("fail: nao existe grafite no bico");
+        } else {
+            int desgaste = this.bico.desgastePorFolha();
+            if(desgaste == this.bico.getTamanho()) {
+                System.out.println("warning: grafite acabou");
+            }
+            else if(this.bico.getTamanho() <= TAMANHO_MIN) {
+                System.out.println("warning: grafite acabou");
+            }
+            else if(desgaste < this.bico.getTamanho()) {
+                bico.setTamanho(bico.getTamanho() - desgaste);
+                if(bico.getTamanho() < TAMANHO_MIN) {
+                    bico.setTamanho(TAMANHO_MIN);
+                    System.out.println("fail: folha incompleta");
+                    System.out.println("warning: grafite acabou");
+                } else if(bico.getTamanho() == TAMANHO_MIN) {
+                    System.out.println("warning: grafite acabou");
+                }
+            }
+        }
+    }
 }
