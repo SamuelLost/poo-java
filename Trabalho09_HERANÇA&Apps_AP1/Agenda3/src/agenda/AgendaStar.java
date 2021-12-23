20bb5873d1ff6e7f21b1be16540849f2c04ba282
SamuelLost <samuelhenriq12@gmail.com>
2021-12-23 13:56:01 -0300

    Add documentation :memo:

--- Trabalho09_HERANÇA&Apps_AP1/Agenda3/src/agenda/AgendaStar.java
+++ Trabalho09_HERANÇA&Apps_AP1/Agenda3/src/agenda/AgendaStar.java
@@ -7,18 +7,33 @@ import contact.Contact;
 import contact.ContactStar;
 public class AgendaStar extends Agenda {
     //SE o contato existir e for do tipo ContatoStar altere o atributo star dele
+    /**
+     * Método que favorita o contato caso ele já exista e seja instância de
+     * ContatoStar. Usando o método findContact(name) para encontrar o contato
+     * pelo nome. Se existir, altera o valor da variável star.
+     * @param name - nome do contato
+     * @param value - true para star, false para unstar
+     */
     public void star(String name, boolean value) {
-        if(findContact(name) != null) {
-            Contact contc = super.findContact(name);
-            //ContactStar star = new ContactStar(contc.getName(), contc.getFones(), value);
+        Contact contc = super.findContact(name);
+        if((contc != null) && (contc instanceof ContactStar)) {
             ((ContactStar) contc).setStar(value);
-            super.rmContact(name);
-            super.addContact(((ContactStar) contc));
         } else {
             System.out.printf("fail: contato %s nao existe\n", name);
         }
     }
+
     //filtre em uma nova lista apenas os contatos que forem do tipo ContatoStar e que estão favoritados
+    /**
+     * O método cria uma lista auxilia de modo que nela seja adicionado
+     * apenas os contatos que foram marcados com star. Para isso, é preciso
+     * percorrer a lista de contatos através do FOR-EACH pegando o método
+     * getContacts() da classe pai (Agenda). Além disso é preciso verificar
+     * se o contato é instancia de ContactStar e se o atributo star é true, 
+     * caso respeite isso o contato é adicionado na lista auxiliar e no final
+     * do método a lista é retornada.
+     * @return lista com os contatos star
+     */
     public List<Contact> getStarred() {
         List<Contact> aux = new ArrayList<>();
         for(Contact contato : super.getContacts()) {
@@ -32,10 +47,4 @@ public class AgendaStar extends Agenda {
         return aux;
 
     }
-
-    /* @Override
-    public String toString() {
-        return super.toString();
-    } */
-    
 }
--- Trabalho09_HERANÇA&Apps_AP1/Agenda3/src/contact/ContactStar.java
+++ Trabalho09_HERANÇA&Apps_AP1/Agenda3/src/contact/ContactStar.java
@@ -2,19 +2,36 @@ package contact;
 import java.util.List;
 import fone.Fone;
 
-
 public class ContactStar extends Contact {
     private boolean star;
     //contrutor default
+    /**
+     * Construtor da classe ContactStar que chamar o construtor
+     * da classe pai Contact
+     * @param name - nome do contato
+     * @param fones - telefones do contato
+     * @param star - se o contato é favorito ou não
+     */
     public ContactStar(String name, List<Fone> fones, boolean star) {
         super(name, fones);
         setStar(star);
     }
+    
     //muda o prefixo e o valor de star
+    /**
+     * Método para setar a variável booleana star e 
+     * mudar o prefixo da classe pai
+     * @param value - true se for star, false se não
+     */
     public void setStar(boolean value) {
         super.prefix = (value) ? "@" : "-";
         star = value;
     }
+
+    /**
+     * GET
+     * @return true ou false
+     */
     public boolean getStar() {
         return star;
     }
